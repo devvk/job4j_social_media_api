1c3988691c4e9dc16351bfc1c4b359155ff41177
devvk <info@sekretov.net>
2026-06-11 16:21:46 +0200

    Именованные запросы [#505063]

--- src/main/java/ru/job4j/socialmedia/repository/PostRepository.java
+++ src/main/java/ru/job4j/socialmedia/repository/PostRepository.java
@@ -1,7 +1,18 @@
 package ru.job4j.socialmedia.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.job4j.socialmedia.model.Post;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findAllByAuthorIdOrderByCreatedAtDesc(Long authorId);
+
+    List<Post> findAllByCreatedAtBetween(LocalDateTime start, LocalDateTime end);
+
+    Page<Post> findAllByOrderByCreatedAtDesc(Pageable pageable);
 }
