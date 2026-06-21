39277646b84a26daa15c4f096667a4214d61b259
devvk <info@sekretov.net>
2026-06-21 18:35:46 +0200

    4. Добавление SpringSecurity в проект Social_media [#505121]

--- src/main/java/ru/job4j/socialmedia/Application.java
+++ src/main/java/ru/job4j/socialmedia/Application.java
@@ -2,7 +2,10 @@ package ru.job4j.socialmedia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
+@EnableScheduling
 @SpringBootApplication
 public class Application {
 
@@ -10,4 +13,10 @@ public class Application {
         SpringApplication.run(Application.class, args);
         System.out.println("http://localhost:8080/swagger-ui/index.html");
 	}
+
+    @Scheduled(fixedRate = 10_000)
+    public void reportCurrentTime() {
+        System.out.println("Текущее время: " + new java.util.Date());
+        System.out.println("Thread name: " + Thread.currentThread().getName());
+    }
 }
